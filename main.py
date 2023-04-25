3acd4dc3704abf1e26346f8135c4c49692a14f04
mikumifa <1055069518@qq.com>
2023-04-25 22:30:10 +0800

    存储cookie登录

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from selenium.webdriver.support import expected_conditions as EC
 
 engine = pyttsx3.init()
 buy_url = "https://show.bilibili.com/platform/detail.html?id=72320&from=pc_ticketlist"
+tick_xpath = '/html/body/div/div[2]/div[2]/div[2]/div[4]/ul[1]/li[2]/div[1]'
+# buy_url = "https://show.bilibili.com/platform/detail.html?id=71352&from=pc_ticketlist"
+# tick_xpath = ''
 # 加载配置文件
 with open('./config.json', 'r') as f:
     config = json.load(f)
@@ -24,10 +27,11 @@ def voice(message):
 
 TargetTime = "2023-04-16 16:39:00.00000000"  # 设置抢购时间
 WebDriver = webdriver.Chrome()
-wait = WebDriverWait(WebDriver, 1)
+wait = WebDriverWait(WebDriver, 0.5)
 if len(config["bilibili_cookies"]) == 0:
     WebDriver.get(
         buy_url)  # 输入目标购买页面
+    WebDriver.maximize_window()
     time.sleep(1)
     WebDriver.find_element(By.CLASS_NAME, "nav-header-register").click()
     print("请登录")
@@ -43,7 +47,6 @@ if len(config["bilibili_cookies"]) == 0:
 else:
     WebDriver.get(
         buy_url)  # 输入目标购买页面
-    time.sleep(1)
     for cookie in config["bilibili_cookies"]:  # 利用cookies登录账号
         my_cookie = {
             'domain': cookie['domain'],
@@ -61,10 +64,10 @@ else:
             cookie['secure'] = cookie['secure']
 
         WebDriver.add_cookie(
-           my_cookie
+            my_cookie
         )
+        WebDriver.maximize_window()
 
-    time.sleep(1)
 while True:
     now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
     print(now + "     " + TargetTime)
@@ -74,9 +77,10 @@ while True:
 
 while True:
     try:
-        element = wait.until(EC.visibility_of_element_located(
-            (By.XPATH, '/html/body/div/div[2]/div[2]/div[2]/div[4]/ul[1]/li[2]/div[1]')))
-        element.click()
+        if tick_xpath != "":
+            element = wait.until(EC.visibility_of_element_located(
+                (By.XPATH, tick_xpath)))
+            element.click()
         # 等待抢票按钮出现并可点击
         element = wait.until(EC.element_to_be_clickable(
             (By.CLASS_NAME, 'product-buy.enable')))
@@ -84,15 +88,19 @@ while True:
         element.click()
         # time.sleep(5)
         print("进入购买页面成功")
+        break
     except:
         WebDriver.refresh()
         continue
 
-    try:
-        WebDriver.find_element(By.CLASS_NAME, "confirm-paybtn.active").click()
-        print("订单创建完成，请在一分钟内付款")
 
-        voice('抢到票了，速归！')
-        time.sleep(60)
+element = None
+while element is None:
+    try:
+        element = WebDriver.find_element(By.XPATH, '//*[@id="app"]/div[2]/div/div[5]/div[2]')
     except:
-        print("无法点击创建订单")
+        continue
+element.click()
+print("订单创建完成，请在一分钟内付款")
+voice('抢到票了，速归！')
+time.sleep(60)
