7d72922354284e5d0f9ab4df14d71f40c6fe1abb
mikumifa <1055069518@qq.com>
2023-11-11 11:59:25 +0800

    添加template

--- config.py
+++ config.py
@@ -3,20 +3,7 @@ import os
 import sys
 
 
-def configure_global_logging():
-    # 获取打包后可执行文件的路径
-    if getattr(sys, 'frozen', False):
-        application_path = sys._MEIPASS
-    else:
-        application_path = os.path.dirname(os.path.abspath(__file__))
 
-    global_logger = logging.getLogger()
-    global_logger.setLevel(logging.DEBUG)
-    log_file_path = os.path.join(application_path, 'log', 'log.txt')
-    file_handler = logging.FileHandler(log_file_path)
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    file_handler.setFormatter(formatter)
-    global_logger.addHandler(file_handler)
 
 
 # 获取图标文件的路径
--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import logging
+import sys
 import time
 import tkinter as tk
+import logging
+import os
+import sys
 
-from config import configure_global_logging
 from menu.TicketOptions import TicketOptionsApp
+
+
+def configure_global_logging():
+    # 获取打包后可执行文件的路径
+    if getattr(sys, 'frozen', False):
+        application_path = sys._MEIPASS
+    else:
+        application_path = os.path.dirname(os.path.abspath(__file__))
+
+    global_logger = logging.getLogger()
+    global_logger.setLevel(logging.DEBUG)
+    log_file_path = os.path.join(application_path, 'log', 'log.txt')
+    file_handler = logging.FileHandler(log_file_path)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    global_logger.addHandler(file_handler)
+
 
 if __name__ == '__main__':
     configure_global_logging()
