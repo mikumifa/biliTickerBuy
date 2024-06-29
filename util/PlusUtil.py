104ab284078ec4f892062a1b187075f3f26e0ca7
mikumifa <1055069518@qq.com>
2024-06-29 14:30:37 +0800

    feat: plusplus功能添加

---
+++ util/PlusUtil.py
@@ -0,0 +1,21 @@
+import json
+
+import loguru
+import requests
+
+
+def send_message(token, content, title):
+    try:
+        url = "http://www.pushplus.plus/send"
+        headers = {
+            "Content-Type": "application/json"
+        }
+
+        data = {
+            "token": token,
+            "content": content,
+            "title": title
+        }
+        requests.post(url, headers=headers, data=json.dumps(data))
+    except Exception as e:
+        loguru.logger.info("PlusPlus消息发送失败")
