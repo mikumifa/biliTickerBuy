5d75a29b9f7febc886511ee6812da8276aab98c9
WittF <WittF@qq.com>
2024-08-05 13:04:06 +0800

    chore: Update problems_tab with project resources and feedback links

--- tab/problems.py
+++ tab/problems.py
@@ -3,8 +3,13 @@ import gradio as gr
 
 def problems_tab():
     gr.Markdown("""
-
-本项目使用说明书 [使用说明书](https://github.com/mikumifa/biliTickerBuy/wiki/%E6%8A%A2%E7%A5%A8%E8%AF%B4%E6%98%8E)
-
-本项目的提问专区 [提问专区](https://github.com/mikumifa/biliTickerBuy/discussions)
-""")
+- **项目资源**
+  - **项目主页**：[mikumifa/biliTickerBuy](https://github.com/mikumifa/biliTickerBuy)
+  - **讨论区**：[Discussions](https://github.com/mikumifa/biliTickerBuy/discussions)
+- **问题反馈**
+  - [通用问题](https://github.com/mikumifa/biliTickerBuy/issues)
+  - [漏洞反馈](https://github.com/mikumifa/biliTickerBuy/issues/new?assignees=&labels=bug%3F&projects=&template=bug-report.yml&title=%5BBug%5D%3A+)
+  - [需求建议](https://github.com/mikumifa/biliTickerBuy/issues/new?assignees=&labels=enhancement&projects=&template=feature-request.yml&title=%5BFeature%5D%3A+)
+- **项目文档**
+  - [抢票说明书](https://github.com/mikumifa/biliTickerBuy/wiki/%E6%8A%A2%E7%A5%A8%E8%AF%B4%E6%98%8E)
+    """)
