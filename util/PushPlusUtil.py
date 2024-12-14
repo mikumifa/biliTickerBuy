c4e3d0762178975159fe9efe82e3c633e8a5413e
一之濑亚子 <61614535+imouup@users.noreply.github.com>
2024-12-14 17:26:32 +0800

    Revert "fix: 删除playsound #448"

--- util/PushPlusUtil.py
+++ util/PushPlusUtil.py
@@ -1,8 +1,11 @@
 import json
+import os
 
 import loguru
+import playsound
 import requests
 
+from config import TEMP_PATH
 
 
 def send_message(token, content, title):
@@ -22,3 +25,5 @@ def send_message(token, content, title):
         loguru.logger.info("PushPlus消息发送失败")
 
 
+if __name__ == '__main__':
+    playsound.playsound(os.path.join(TEMP_PATH, "default.mp3"))
