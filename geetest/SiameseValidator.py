1c8ebc436838f62c67528e791ea4e0cfdcfe839f
mikumifa <1055069518@qq.com>
2025-03-28 16:29:42 +0800

    fix: dynimport bili_ticket_gt_python

--- geetest/SiameseValidator.py
+++ geetest/SiameseValidator.py
@@ -1,57 +1,43 @@
-import concurrent.futures
 import json
 import os.path
 import re
 import time
 
-from util.dynimport import bili_ticket_gt_python
 import cv2
 import loguru
 import numpy as np
 import onnxruntime
 import requests
+from scipy.optimize import linear_sum_assignment
 
 from const import APP_PATH
 from geetest.Validator import Validator, test_validator
-
-
-def process_box(box, image_data_1, img, preprocess_image, Siamese, result_list):
-    if [box[0], box[1]] in result_list:
-        return None
-    cropped = img[box[1]: box[1] + box[3], box[0]: box[0] + box[2]]
-    image_data_2 = preprocess_image(cropped)
-    inputs = {'input': image_data_1, "input.53": image_data_2}
-    output = Siamese.run(None, inputs)
-    output_sigmoid = 1 / (1 + np.exp(-output[0]))
-    return output_sigmoid[0][0], [box[0], box[1]]
+from util.dynimport import bili_ticket_gt_python
 
 
 # https://github.com/Amorter/biliTicker_gt/blob/f378891457bb78bcacf181eaf642b11f5543b4e0/src/click.rs#L166
-#
+
 class Model:
     def __init__(self, ):
-        self.img = None
         self.yolo = onnxruntime.InferenceSession(os.path.join(APP_PATH, "geetest", "model", "yolo.onnx"))
-        self.Siamese = onnxruntime.InferenceSession(os.path.join(APP_PATH, "geetest", "model", "siamese.onnx"))
-        self.classes = ["big", "small"]
-        self.color_palette = np.random.uniform(0, 255, size=(len(self.classes), 3))
+        self.siamese = onnxruntime.InferenceSession(os.path.join(APP_PATH, "geetest", "model", "siamese.onnx"))
+        self.size = (224, 224)
 
-    def detect(self, img: bytes):
+    def detect(self, img) -> (list[bytes], list[list[int]]):
         confidence_thres = 0.8
         iou_thres = 0.8
         model_inputs = self.yolo.get_inputs()
         input_shape = model_inputs[0].shape
         input_width = input_shape[2]
         input_height = input_shape[3]
-        self.img = cv2.imdecode(np.frombuffer(img, np.uint8), cv2.IMREAD_ANYCOLOR)
-        img_height, img_width = self.img.shape[:2]
-        img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
+        origin_img = cv2.imdecode(np.frombuffer(img, np.uint8), cv2.IMREAD_ANYCOLOR)
+        img_height, img_width = origin_img.shape[:2]
+        img = cv2.cvtColor(origin_img, cv2.COLOR_BGR2RGB)
         img = cv2.resize(img, (input_height, input_width))
         image_data = np.array(img) / 255.0
         image_data = np.transpose(image_data, (2, 0, 1))
         image_data = np.expand_dims(image_data, axis=0).astype(np.float32)
-        input = {model_inputs[0].name: image_data}
-        output = self.yolo.run(None, input)
+        output = self.yolo.run(None, {model_inputs[0].name: image_data})
         outputs = np.transpose(np.squeeze(output[0]))
         rows = outputs.shape[0]
         boxes, scores, class_ids = [], [], []
@@ -71,48 +57,40 @@ class Model:
                 scores.append(max_score)
                 boxes.append([left, top, width, height])
         indices = cv2.dnn.NMSBoxes(boxes, scores, confidence_thres, iou_thres)
-        new_boxes = [boxes[i] for i in indices]
-        small_imgs, big_img_boxes = {}, []
-        for i in new_boxes:
-            cropped = self.img[i[1]: i[1] + i[3], i[0]: i[0] + i[2]]
+        ret_boxes = sorted([boxes[i] for i in indices], key=lambda x: x[0])
+        text_imgs = []
+        text_boxes = []
+        bg_imgs = []
+        bg_boxes = []
+        for i in ret_boxes:
+            cropped = origin_img[i[1]: i[1] + i[3], i[0]: i[0] + i[2]]
             if cropped.shape[0] < 35 and cropped.shape[1] < 35:
-                small_imgs[i[0]] = cropped
+                text_imgs.append(cropped.astype(np.float32))
+                text_boxes.append(i)
             else:
-                big_img_boxes.append(i)
-        return small_imgs, big_img_boxes
+                bg_imgs.append(cropped.astype(np.float32))
+                bg_boxes.append(i)
+        return text_imgs, text_boxes, bg_imgs, bg_boxes
 
-    @staticmethod
-    def preprocess_image(img, size=(105, 105)):
-        img_resized = cv2.resize(img, size)
-        img_normalized = np.array(img_resized) / 255.0
-        img_transposed = np.transpose(img_normalized, (2, 0, 1))
-        img_expanded = np.expand_dims(img_transposed, axis=0).astype(np.float32)
-        return img_expanded
+    def match(self, text_imgs, bg_imgs, bg_imgs_box):
+        n = len(text_imgs)
 
-    def siamese(self, small_imgs, big_img_boxes):
-        preprocessed_small_imgs = {i: self.preprocess_image(small_imgs[i]) for i in sorted(small_imgs)}
-        result_list = []
-        output_res = []
-        for i in sorted(preprocessed_small_imgs):
-            image_data_1 = preprocessed_small_imgs[i]
-            box_score = []
-            with concurrent.futures.ThreadPoolExecutor() as executor:
-                futures = [
-                    executor.submit(process_box, box, image_data_1, self.img, self.preprocess_image, self.Siamese,
-                                    result_list)
-                    for box in big_img_boxes if [box[0], box[1]] not in result_list]
-                for future in concurrent.futures.as_completed(futures):
-                    result = future.result()
-                    if result:
-                        box_score.append(result)
-            if box_score:
-                max_score_pair = max(box_score, key=lambda x: x[0])
-                if max_score_pair[0] >= 0.5:
-                    output_res.append(max_score_pair[0])
-                    result_list.append(max_score_pair[1])
-                else:
-                    break
-        return result_list, output_res
+        text_imgs = np.stack(
+            [(cv2.resize(img, self.size) / 255).transpose(2, 0, 1) for img in text_imgs])  # (n, C, H, W)
+        bg_imgs = np.stack([(cv2.resize(img, self.size) / 255).transpose(2, 0, 1) for img in bg_imgs])  # (n, C, H, W)
+
+        text_batch = np.repeat(text_imgs[:, None, :, :, :], n, axis=1).reshape(n * n, 3, self.size[0],
+                                                                               self.size[1])  # (n*n, C, H, W)
+        bg_batch = np.repeat(bg_imgs[None, :, :, :, :], n, axis=0).reshape(n * n, 3, self.size[0],
+                                                                           self.size[1])  # (n*n, C, H, W)
+        inputs = {'img1': text_batch, 'img2': bg_batch}
+        outputs = self.siamese.run(None, inputs)[0]
+        similarity_matrix = outputs.reshape(n, n)
+
+        row_ind, col_ind = linear_sum_assignment(-similarity_matrix)
+        result_list = sorted([(i, bg_imgs_box[j]) for i, j in zip(row_ind, col_ind)], key=lambda x: x[0])
+        match_scores = [similarity_matrix[i, j] for i, j in zip(row_ind, col_ind)]
+        return result_list, match_scores
 
 
 def download_img(url: str) -> bytes:
@@ -162,14 +140,12 @@ class SiameseValidator(Validator):
             try:
                 before_calculate_key = time.time()
                 pic_content = download_img(args)
-                small_img, big_img = self.model.detect(pic_content)
-                if len(big_img) != len(small_img) or len(small_img) == 1:
+                text_imgs, text_boxes, bg_imgs, bg_boxes = self.model.detect(pic_content)
+                if len(text_boxes) != len(bg_boxes) or len(text_boxes) == 1 or len(bg_boxes) == 1:
                     raise Exception("fast retry")
-                result_list, output_res = self.model.siamese(small_img, big_img)
-                if len(result_list) != len(small_img) or len(result_list) == 1:
-                    raise Exception("fast retry")
+                result_list, output_res = self.model.match(text_imgs, bg_imgs, bg_boxes)
                 point_list = []
-                for i in result_list:
+                for idx, i in result_list:
                     left = str(round((i[0] + 30) / 333 * 10000))
                     top = str(round((i[1] + 30) / 333 * 10000))
                     point_list.append(f"{left}_{top}")
@@ -183,10 +159,9 @@ class SiameseValidator(Validator):
                     raise Exception("生成错误")
                 loguru.logger.info(f"本地验证码过码成功")
                 loguru.logger.debug(f"{output_res}")
-
                 return validate
             except Exception as e:
-                loguru.logger.exception(e)
+                loguru.logger.info(e)
                 args = refresh(gt, challenge)
 
 
